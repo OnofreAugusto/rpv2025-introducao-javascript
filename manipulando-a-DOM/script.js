2ab900873c35fe406567695c56e5e0fb2936c232
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-12-11 22:26:08 -0300

    feat: final DOM

--- manipulando-a-DOM/script.js
+++ manipulando-a-DOM/script.js
@@ -19,3 +19,63 @@ bodyElement.appendChild(mainElement)
 mainElement.appendChild(h1Element)
 mainElement.appendChild(pElement)
 mainElement.appendChild(spanElement)
+
+function digaOla() {
+    alert('Olá, seja bem-vindo !')
+}
+
+function selecaoFiat() {
+    alert('Olá, você selecionou o Fiat.')
+}
+
+function resetarCoresDeFundoPadrao(){
+    cardFiat.classList.add('padrao')
+    cardFerrari.classList.add('padrao')
+    cardJeep.classList.add('padrao')
+    cardRam.classList.add('padrao')
+}
+
+function alteraCardAtivo(botaoClicado){
+    const cardClicado = document.getElementById(botaoClicado)
+    cardClicado.classList.remove('padrao')
+    cardClicado.classList.toggle('ativo')
+    
+    // if(estaAtivo){
+    //     return cardClicado.classList.remove('ativo')
+    // }
+    
+    // // console.log('cardClicado', cardClicado.classList['value'])
+    // // console.log('cardClicado', cardClicado)
+    // cardClicado.classList.add('ativo')
+
+}
+
+const cardFiat = document.getElementById("fiat")
+const cardFerrari = document.getElementById("ferrari")
+const cardJeep = document.getElementById("jeep")
+const cardRam = document.getElementById("ram")
+
+cardFiat.addEventListener('click', () => {
+    resetarCoresDeFundoPadrao()
+    alteraCardAtivo('fiat')
+    // cardFiat.style.backgroundColor = 'red'
+})
+
+cardFerrari.addEventListener('click', () => {
+    resetarCoresDeFundoPadrao()
+    alteraCardAtivo('ferrari')
+    // cardFerrari.style.backgroundColor = 'red'
+})
+
+cardJeep.addEventListener('click', () => {
+    resetarCoresDeFundoPadrao()
+    alteraCardAtivo('jeep')
+    // cardJeep.style.backgroundColor = 'red'
+})
+
+cardRam.addEventListener('click', () => {
+    resetarCoresDeFundoPadrao()
+    alteraCardAtivo('ram')
+    // cardRam.style.backgroundColor = 'red'
+})
+
